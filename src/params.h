ff1e6719b26666a745d98219f82320d93270c888
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2016-06-15 14:14:09 +0200

    move _XOPEN_SOURCE definition to config.h
    this ensures it is always defined early enough

--- src/params.h
+++ src/params.h
@@ -16,10 +16,6 @@
 // writing, the most current API version is 26
 #define FUSE_USE_VERSION 26
 
-// need this to get pwrite().  I have to use setvbuf() instead of
-// setlinebuf() later in consequence.
-#define _XOPEN_SOURCE 500
-
 // maintain bbfs state in here
 #include <limits.h>
 #include <stdio.h>
