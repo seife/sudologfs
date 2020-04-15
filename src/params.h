3a15b26bdc8a53c7ad3fd1f4ccbdf5a9aa64e1f6
Erik Inge Bolsø <erik.inge.bolso@modirum.com>
2020-04-15 14:03:29 +0200

    update to libfuse api 35

--- src/params.h
+++ src/params.h
@@ -13,8 +13,8 @@
 
 // The FUSE API has been changed a number of times.  So, our code
 // needs to define the version of the API that we assume.  As of this
-// writing, the most current API version is 26
-#define FUSE_USE_VERSION 26
+// writing, the most current API version is 35
+#define FUSE_USE_VERSION 35
 
 // maintain bbfs state in here
 #include <limits.h>
